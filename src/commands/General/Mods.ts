5618c175695d3e849b2ddc998b8eaf458c22113b
tonikawa-kiss <93930712+tonikawa-kiss@users.noreply.github.com>
2022-05-09 14:14:13 +0530

    Add files via upload

---
+++ src/commands/General/Mods.ts
@@ -0,0 +1,33 @@
+import MessageHandler from "../../Handlers/MessageHandler";
+import BaseCommand from "../../lib/BaseCommand";
+import WAClient from "../../lib/WAClient";
+import { ISimplifiedMessage } from "../../typings";
+
+export default class Command extends BaseCommand {
+  constructor(client: WAClient, handler: MessageHandler) {
+    super(client, handler, {
+      command: "mods",
+      description: "Displays the Moderators' contact info",
+      category: "general",
+      usage: `${client.config.prefix}mods`,
+      aliases: ["moderators", "mod", "owner"],
+    });
+  }
+
+  run = async (M: ISimplifiedMessage): Promise<void> => {
+    if (!this.client.config.mods || !this.client.config.mods[0])
+      return void M.reply("*[UNMODERATED]*");
+    const filteredMap = this.client.config.mods
+      .map((mod) => this.client.getContact(mod))
+      .filter((user) => user);
+    let text = "🚀DREADED-BOT OWNERS🚀\n\n";
+    filteredMap.forEach(
+      (user, index) =>
+        (text += `#${index + 1}\n🎉 *Username: ${
+          user.notify || user.vname || user.name || "null"
+        }*\n🏮 *Contact: https://wa.me/+${user?.jid?.split("@")[0]}*\n\n`)
+    );
+    text += `\n 🚀DREADED-BOTTO🚀`;
+    return void M.reply(text);
+  };
+}
---
+++ src/commands/General/support.ts
@@ -0,0 +1,29 @@
+import { MessageType, Mimetype } from "@adiwajshing/baileys";
+import MessageHandler from "../../Handlers/MessageHandler";
+import BaseCommand from "../../lib/BaseCommand";
+import WAClient from "../../lib/WAClient";
+import { ISimplifiedMessage } from "../../typings";
+
+export default class Command extends BaseCommand {
+  constructor(client: WAClient, handler: MessageHandler) {
+    super(client, handler, {
+      command: "support",
+      aliases: ["support"],
+      description: "Gets the support group links",
+      category: "general",
+      usage: `${client.config.prefix}Support`,
+      baseXp: 10,
+    });
+  }
+
+  run = async (M: ISimplifiedMessage): Promise<void> => {
+    await this.client.sendMessage(
+      M.sender.jid,
+      ` _*✨WELL, IF THAT WAS THE CASE THEN  ✨*_\n _*ZERO TWO SUPPORT GROUP*_:   https://chat.whatsapp.com/Fnk9vRRcSS8L22L8Bq1GyZ   🛡 NSFW 🛡 https://chat.whatsapp.com/KC43a4UytJZ00XE0WXVz7v `,
+
+      MessageType.text
+    );
+
+    return void M.reply("Sent you a personal message regarding this.");
+  };
+}
